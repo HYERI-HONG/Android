d9a48863d4fdcf6efa8535a18485f5f4c7d29df7
HYERI-HONG <rusa12615059@gmail.com>
2018-09-04 12:53:43 +0900

    calc complete

--- MyApp1/app/src/main/java/app/hong/com/myapp1/MainActivity.java
+++ MyApp1/app/src/main/java/app/hong/com/myapp1/MainActivity.java
@@ -10,61 +10,101 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        class Calc{
+            String rs="",symbol="";
+            int num, sum;
+
+            public int getSum() { return sum; }
+            public String getRs() { return rs; }
+
+            public void setNum(int num) {
+                this.num = num;
+                this.rs+=num;
+                this.setSum(num);
+            }
+            public void setSymbol(String symbol) {
+                this.symbol = symbol;
+                this.rs+=symbol.equals("=")?symbol+this.sum:symbol;
+            }
+            public void setSum(int num) {
+                switch(this.symbol){
+                    case "+": this.sum+=num;break;
+                    case "-": this.sum-=num;break;
+                    case "/": this.sum/=num;break;
+                    case "*": this.sum*=num;break;
+                    case "": this.sum = num;break;
+                }
+            }
+            public void clear(){
+                this.symbol="";
+                this.rs="";
+                this.sum=0;
+                this.num=0;
+            }
+
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Context ctx = MainActivity.this;
+        final EditText num = findViewById(R.id.num);
+        final TextView result= findViewById(R.id.num);
+        final Calc calc=new Calc();
         findViewById(R.id.plus_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText num1 = findViewById(R.id.num1);
-                EditText num2 = findViewById(R.id.num2);
-                TextView result= findViewById(R.id.result);
-                result.setText(String.valueOf(
-                        Integer.parseInt(num1.getText().toString())
-                        +Integer.parseInt(num2.getText().toString())
-                        ));
-                //Toast.makeText(ctx, "입력한 숫자값:"+sNum1,Toast.LENGTH_LONG).show();
+                calc.setNum(Integer.parseInt(num.getText().toString()));
+                calc.setSymbol("+");
+                //num.setSelection(num.getText().length());
+                result.setText("");
+                Toast.makeText(ctx, "계산식:"+calc.getRs()+"누적값"+String.valueOf(calc.getSum()),Toast.LENGTH_LONG).show();
             }
         });
         findViewById(R.id.min_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText num1 = findViewById(R.id.num1);
-                EditText num2 = findViewById(R.id.num2);
-                TextView result= findViewById(R.id.result);
-                result.setText(String.valueOf(
-                        Integer.parseInt(num1.getText().toString())
-                        -Integer.parseInt(num2.getText().toString())
-                        ));
+                calc.setNum(Integer.parseInt(num.getText().toString()));
+                calc.setSymbol("-");
+                result.setText("");
+                Toast.makeText(ctx, "계산식:"+calc.getRs()+"누적값"+String.valueOf(calc.getSum()),Toast.LENGTH_LONG).show();
             }
         });
         findViewById(R.id.mult_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText num1 = findViewById(R.id.num1);
-                EditText num2 = findViewById(R.id.num2);
-
-                TextView result= findViewById(R.id.result);
-                result.setText(String.valueOf(
-                        Integer.parseInt(num1.getText().toString())
-                        *Integer.parseInt(num2.getText().toString())
-                        ));
+                calc.setNum(Integer.parseInt(num.getText().toString()));
+                calc.setSymbol("*");
+                result.setText("");
+                Toast.makeText(ctx, "계산식:"+calc.getRs()+"누적값"+String.valueOf(calc.getSum()),Toast.LENGTH_LONG).show();
             }
         });
         findViewById(R.id.div_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText num1 = findViewById(R.id.num1);
-                EditText num2 = findViewById(R.id.num2);
-                TextView result= findViewById(R.id.result);
-                result.setText(String.valueOf(
-                        Integer.parseInt(num1.getText().toString())
-                        /Integer.parseInt(num2.getText().toString())
-                        ));
+                calc.setNum(Integer.parseInt(num.getText().toString()));
+                calc.setSymbol("/");
+                result.setText("");
+                Toast.makeText(ctx, "계산식:"+calc.getRs()+"누적값"+String.valueOf(calc.getSum()),Toast.LENGTH_LONG).show();
             }
         });
+        findViewById(R.id.rs_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calc.setNum(Integer.parseInt(num.getText().toString()));
+                calc.setSymbol("=");
+                result.setText(calc.getRs());
+            }
+        });
+        findViewById(R.id.c_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("");
+                calc.clear();
+                Toast.makeText(ctx, "누적값:"+String.valueOf(calc.getSum()),Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
---
+++ MyApp2/app/src/main/java/app/hong/com/myapp2/BMI.java
@@ -0,0 +1,13 @@
+package app.hong.com.myapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BMI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bmi);
+    }
+}
